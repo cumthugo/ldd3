5b25adcdc716bc5f0713fb7d2209103aa1c21ff4
Yajun Fu <fuyajun1983cn@163.com>
2015-11-14 10:10:44 +0800

    use proc_create instead

--- misc-modules/seq.c
+++ misc-modules/seq.c
@@ -94,9 +94,11 @@ static int ct_init(void)
 {
 	struct proc_dir_entry *entry;
 
-	entry = create_proc_entry("sequence", 0, NULL);
-	if (entry)
-		entry->proc_fops = &ct_file_ops;
+        entry = proc_create("sequence", S_IFREG|S_IRUGO, NULL, &ct_file_ops);
+	if (!entry) {
+            printk(KERN_ERR "create sequence entry failed!\n");
+            return -1;
+        }
 	return 0;
 }
 
